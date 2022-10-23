d28feb2428ba0f4fe475eee6e637cdb6ea55b575
VuNguyen <vietvu96lean@gmail.com>
2022-10-23 22:36:07 +0700

    23/10/2022

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import {useSelector,useDispatch} from 'react-redux'
 import ProductCard from '../../components/ProductCard';
 import { AppDispatch, RootState } from '../../redux/configStore'
 import { getAllProductApi, Product } from '../../redux/reducers/productReducer';
+import Carousel from './Carousel';
 type Props = {}
 
 export default function Home({}: Props) {
@@ -16,10 +17,47 @@ export default function Home({}: Props) {
         //Gọi api
         const action = getAllProductApi();
         dispatch(action);
-    },[])
+    },[]);
+    const logo =  require("../../assets/img/Logo.png");
+    const html = require("../../assets/img/html2.png");
+    const css = require ("../../assets/img/css.png")
+    const js = require ("../../assets/img/js.webp")
 
 
   return (
+    <div>
+
+    <div  id="carouselExampleControls" className="carousel slide " data-bs-ride="carousel">
+    <div className="carousel-inner carousel_1">
+      <div className="carousel-item active carousel_2">
+        <img src= {logo}  className="d-block w-100" alt="..."/>
+        <div>
+          <h1>Tổng quan về khóa học </h1>
+        </div>
+      </div>
+      <div className="carousel-item carousel_2">
+        <img src= {html} className="d-block w-100" alt="..."/>
+        <h1>Kiến thức về HTML</h1>
+      </div>
+      <div className="carousel-item carousel_2">
+        <img src= {css} className="d-block w-100" alt="..."/>
+        <h1>Kiến thức về CSS</h1>
+      </div>
+      <div className="carousel-item carousel_2">
+        <img  src= {js} className="d-block w-100" alt="..."/>
+        <h1>Kiến thức về JavaScript</h1>
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+    
     <div className='container '> 
      <div className='ProductFeature'>
         <div className='ProductText'>
@@ -33,6 +71,7 @@ export default function Home({}: Props) {
                 </div>
             })}
         </div>
+    </div>
     </div>
   )
 }
