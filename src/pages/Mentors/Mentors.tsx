402eebe054b984558e055d5a6365fd304c765bf7
VuNguyen <vietvu96lean@gmail.com>
2022-10-31 06:09:07 +0700

    31.10

--- src/pages/Mentors/Mentors.tsx
+++ src/pages/Mentors/Mentors.tsx
@@ -1,116 +1,122 @@
 import React from 'react'
 import './../../assets/scss/pages/_mentors.scss'
 
+
 type Props = {}
 
-export default function Mentors({}: Props) {
+
+
+
+
+export default function Mentors({ }: Props) {
   return (
+     
 
     <div>
       <div className="mentors_title">
         <p>Our Team</p>
-        
+
       </div>
-        <div className="container">
-              <div className="intro_mentors1">
-                  <div className="row">
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="./img//mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="../../img/mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="../../img/mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-
-
-                  </div>
+      <div className="container">
+        <div className="intro_mentors1">
+          <div className="row">
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="./img//mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
               </div>
+            </div>
 
-              <div className="intro_mentors2">
-                  <div className="row">
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="../../img/mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="../../img/mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-
-                    <div className="col-4">
-                        <div className="mentor_profile">
-                            <img src="../../img/mentor/mentor_1.jpg" alt="..." />
-                              <div className="contact_mentor">
-                             <i className="fab fa-facebook-square"></i>
-                             <i className="fab fa-instagram"></i>
-                             <i className="fab fa-twitter"></i>
-                              </div>
-                                <div className="mentor_item">
-                                      <h2>Mentor name</h2>
-                                        <p>Frontend Developer</p>
-                                </div>
-                        </div>       
-                    </div>
-                  </div>
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="../../img/mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
               </div>
+            </div>
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="../../img/mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
         </div>
+
+        <div className="intro_mentors2">
+          <div className="row">
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="../../img/mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="../../img/mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4">
+              <div className="mentor_profile">
+                <img src="../../img/mentor/mentor_1.jpg" alt="..." />
+                <div className="contact_mentor">
+                  <i className="fab fa-facebook-square"></i>
+                  <i className="fab fa-instagram"></i>
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="mentor_item">
+                  <h2>Mentor name</h2>
+                  <p>Frontend Developer</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
